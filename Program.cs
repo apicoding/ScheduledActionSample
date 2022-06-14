02476d5663398c2dd7d86d179f6682d013ed1289
apicoding <apicodingprj@gmail.com>
2022-06-14 21:46:54 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using DIE.ScheduledAction;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ScheduledActionSample
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            var provider = services.BuildServiceProvider();
+            var service = provider.GetRequiredService<ScheduledAction>();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging();
+            services.AddHostedService<ScheduledAction>();
+        }
+    }
+}
